674e137ce7bbcaadb39fb7ed555a16700bda3b09
dSandley20 <danielsandley2@gmail.com>
2022-06-22 16:28:17 -0400

    add link to the kitchen sink

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {Link} from 'react-router'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Link to="/kitchen-sink">Kitchen Sink</Link>
       </header>
     </div>
   );
