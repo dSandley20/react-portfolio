9dc87d1884fa83619f1316e93510e495df037734
dSandley20 <danielsandley2@gmail.com>
2022-07-06 01:22:15 -0400

    Add theme provider to index.js

--- src/index.js
+++ src/index.js
@@ -5,16 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import KitchenSink from './routes/KitchenSink';
+import { ThemeProvider } from '@mui/material/styles';
+import mainTheme from './themes/MainTheme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App/>}/>
-      <Route path='/kitchen-sink' element={<KitchenSink/>}/>
-    </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={mainTheme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App/>} />
+          <Route path='/kitchen-sink' element={<KitchenSink/>} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
