02ef72cbe3dc5e987a89ea6c49601b9fcd2a47b6
dSandley20 <danielsandley2@gmail.com>
2022-07-06 11:04:07 -0400

    pull ContactMe component into Kitchen sink

--- src/routes/KitchenSink.jsx
+++ src/routes/KitchenSink.jsx
@@ -1,16 +1,21 @@
 import DisplayCard from "../components/DisplayCard";
 import NavBar from "../components/NavBar";
+import ContactMe from "../components/ContactMe";
+import Socials from "../components/Socials";
 
 export default function KitchenSink() {
   return (
     <>
       <NavBar />
+
       <DisplayCard
         title={"FireShip"}
         text={
           "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, asperiores."
         }
       />
+
+      <ContactMe />
     </>
   );
 }
