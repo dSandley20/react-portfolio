a9f6ae16aa692c76c98d04594b25fdb0ace1d9ae
dSandley20 <danielsandley2@gmail.com>
2022-07-06 10:05:31 -0400

    offload styles like css

---
+++ src/components/DisplayCard/styles.js
@@ -0,0 +1,11 @@
+//card properties
+const mainCardStyle = {
+  padding: "3rem 5rem",
+  borderRadius: "1rem",
+  backgroundColor: "blue",
+  boxShadow: "0px 10px 30px rgb(0,0,0,0.5)",
+};
+const cardTextStyle = { paddingTop: "1rem", lineHeight: "150% " };
+
+
+module.exports = {mainCardStyle, cardTextStyle}
