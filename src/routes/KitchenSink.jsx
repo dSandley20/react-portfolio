089ea8de25e762480a30733b7d6709858b4b94b0
dSandley20 <danielsandley2@gmail.com>
2022-07-06 11:31:08 -0400

    testing grid spacing with DisplayCards

--- src/routes/KitchenSink.jsx
+++ src/routes/KitchenSink.jsx
@@ -9,10 +9,26 @@ export default function KitchenSink() {
     <>
       <NavBar />
 
-      <Grid container spacing={2}>
-        <Grid item spacing={4}>
+      <Grid container spacing={4}>
+        <Grid item xs={4} sx={{ width: "30%" }}>
           <DisplayCard
             title={"FireShip"}
+            text={
+              "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, asperiores."
+            }
+          />
+        </Grid>
+        <Grid item xs={4} sx={{ width: "30%" }}>
+          <DisplayCard
+            title={"FireShip1"}
+            text={
+              "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, asperiores."
+            }
+          />
+        </Grid>
+        <Grid item xs={4} sx={{ width: "30%" }}>
+          <DisplayCard
+            title={"FireShip2"}
             text={
               "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, asperiores."
             }
