d6857df72d28ff6019c80ada98972c216767ed04
dSandley20 <danielsandley2@gmail.com>
2022-07-06 16:46:01 -0400

    basic blocking for social buttons

--- src/components/Socials/index.jsx
+++ src/components/Socials/index.jsx
@@ -1,3 +1,53 @@
+import { Box, IconButton } from "@mui/material";
+import { container, socialButton } from "./styles";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
 export default function Socials(props) {
-  return <></>;
+  const twitterUrl = "https://twitter.com/NotBluePost";
+  const githubUrl = "https://github.com/dSandley20";
+  const linkedinUrl = "https://www.linkedin.com/in/dsandley20/";
+
+  const urlHandler = (url) => {
+    console.log(url);
+    window.open(url, "_blank");
+  };
+
+  return (
+    <>
+      <Box sx={container}>
+        <Box sx={socialButton}>
+          <IconButton
+            onClick={() => {
+              urlHandler(twitterUrl);
+            }}
+          >
+            <TwitterIcon />
+          </IconButton>
+          {twitterUrl}
+        </Box>
+        <Box sx={socialButton}>
+          <IconButton
+            onClick={() => {
+              urlHandler(githubUrl);
+            }}
+          >
+            <GitHubIcon />
+          </IconButton>
+          {githubUrl}
+        </Box>
+        <Box sx={socialButton}>
+          <IconButton
+            onClick={() => {
+              urlHandler(linkedinUrl);
+            }}
+          >
+            <LinkedInIcon />
+          </IconButton>
+          {linkedinUrl}
+        </Box>
+      </Box>
+    </>
+  );
 }
---
+++ src/components/Socials/styles.js
@@ -0,0 +1,13 @@
+const container = {
+    display: 'flex',
+    flexDirection: 'column'
+}
+
+const socialButton = {
+    justifyContent: 'left'
+}
+
+module.exports = {
+    container,
+    socialButton
+}
