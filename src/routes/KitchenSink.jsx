326b156124d6f916413368f95098d990f7186e74
dSandley20 <danielsandley2@gmail.com>
2022-07-07 15:53:20 -0400

    clean up unused imports currently

--- src/routes/KitchenSink.jsx
+++ src/routes/KitchenSink.jsx
@@ -1,8 +1,4 @@
-import DisplayCard from "../components/DisplayCard";
 import NavBar from "../components/NavBar";
-import ContactMe from "../components/ContactMe";
-import Socials from "../components/Socials";
-import { Grid, Item } from "@mui/material";
 
 export default function KitchenSink() {
   return (
