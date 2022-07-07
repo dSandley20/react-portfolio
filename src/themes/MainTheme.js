e0cb24ce876ab687d4be8c8a680c2618e6c122fd
dSandley20 <danielsandley2@gmail.com>
2022-07-07 10:18:25 -0400

    update theme colors to match nord theme

--- src/themes/MainTheme.js
+++ src/themes/MainTheme.js
@@ -1,17 +1,62 @@
 import { createTheme }  from '@mui/material/styles';
 
-//TODO choose theme colors either synthwave or ide colors
+// colors are based off of the Nord VSCode theme, https://www.nordtheme.com/docs/colors-and-palettes
+// nord0-3 , used for background or text
+// nord4-6 , used for ui elements or text
+// nord7-10 , used for text and code syntax
+// nord11-15, used for highlights
 
 const mainTheme = createTheme({
     palette : {
         type: 'light',
-       
-        accent: {
-            main: "#00aab3"
+        nord0: {
+            main: '#2E3440'
         },
-        highlight: {
-            main: "#FFFFFFF"
-        }
+        nord1: {
+            main: '#3B4252'
+        },
+        nord2: {
+            main: '#434C5E'
+        },
+        nord3: {
+            main: '#4C566A'
+        },
+        nord4: {
+            main: '#D8DEE9'
+        },
+        nord5: {
+            main: '#E5E9F0'
+        },
+        nord6: {
+            main: '#ECEFF4'
+        },
+        nord7: {
+            main: '#8FBCBB'
+        },
+        nord8: {
+            main: '#88C0D0'
+        },
+        nord9: {
+            main: '#81A1C1'
+        },
+        nord10: {
+            main: '#5E81AC'
+        },
+        nord11: {
+            main: '#BF616A'
+        },
+        nord12: {
+            main: '#D08770'
+        },
+        nord13: {
+            main: '#EBCB8B'
+        },
+        nord14: {
+            main: '#A3BE8C'
+        },
+        nord15: {
+            main: '#B48EAD'
+        },
         
     }
 })
