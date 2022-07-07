0bbd09ea407516699de93a982c170c8689ff99a3
dSandley20 <danielsandley2@gmail.com>
2022-07-07 00:04:01 -0400

    add reusable style for icon and lowercase txt

--- src/components/Socials/styles.js
+++ src/components/Socials/styles.js
@@ -7,7 +7,17 @@ const socialButton = {
     justifyContent: 'left'
 }
 
+const buttonStyle = {
+    textTransform: 'none',
+}
+
+const iconStyle = {
+    marginRight: '10px'
+}
+
 module.exports = {
     container,
-    socialButton
+    socialButton,
+    buttonStyle,
+    iconStyle
 }
