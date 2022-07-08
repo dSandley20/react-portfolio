744c5f4937a1141065ac18c99d4c93e67bfeb344
Daniel Sandley <danielsandley2@gmail.com>
2022-07-08 10:54:12 -0400

    create seperate styles for desktop + mobile

--- src/components/ContactMe/styles.js
+++ src/components/ContactMe/styles.js
@@ -1,4 +1,4 @@
-const container = {
+const containerDesktop = {
     borderRadius: "1rem",
     height: '100px',
     borderColor: '#8FBCBB',
@@ -10,4 +10,16 @@ const container = {
     boxShadow: "0px 10px 30px rgb(0,0,0,0.5)",
 }
 
-module.exports = {container}
+const containerMobile = {
+    borderRadius: "1rem",
+    height: '100px',
+    borderColor: '#8FBCBB',
+    marginTop: '10px !important',
+    borderStyle: 'solid',
+    borderWidth: '2px',
+    width: '95%',
+    margin: '0 auto',
+    boxShadow: "0px 10px 30px rgb(0,0,0,0.5)",
+}
+
+module.exports = {containerDesktop, containerMobile}
