ca83d8eff098208ae33ddbccf2faf7b5f5482461
Daniel Sandley <danielsandley2@gmail.com>
2022-07-08 11:29:48 -0400

    start getting formik / formik-mui into ContactMe

--- src/components/ContactMe/index.jsx
+++ src/components/ContactMe/index.jsx
@@ -3,7 +3,8 @@ import { containerDesktop, containerMobile } from "./styles";
 import React, { useState } from "react";
 import { Button, Snackbar, IconButton, Typography, Grid } from "@mui/material";
 import { Formik, Form, Field } from "formik";
-import { string } from "yup";
+import { TextField } from "formik-mui";
+import CustomTextField from "../common/CustomTextField";
 import PropTypes from "prop-types";
 
 export default function ContactMe(props) {
@@ -22,11 +23,47 @@ export default function ContactMe(props) {
         >
           Contact Me
         </Typography>
+        <Formik
+          initialValues={{
+            name: "",
+            email: "",
+            subject: "",
+            message: "",
+          }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = "Required";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+            ) {
+              errors.email = "Invalid email address";
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              setSubmitting(false);
+              alert(JSON.stringify(values, null, 2));
+            }, 500);
+          }}
+        >
+          {({ submitForm, isSubmitting }) => (
+            <Form style={{ padding: "10px" }}>
+              <Field
+                component={CustomTextField}
+                name="name"
+                type="string"
+                label="Name"
+              />
+            </Form>
+          )}
+        </Formik>
       </Box>
     </>
   );
 }
 
 ContactMe.propTypes = {
-  isMobile: PropTypes.string,
+  isMobile: PropTypes.bool,
 };
