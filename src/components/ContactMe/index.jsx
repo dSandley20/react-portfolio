8e4ea8556b04328936858fd877909962e4153059
Daniel Sandley <danielsandley2@gmail.com>
2022-07-08 10:54:37 -0400

    get tittle/subject for ContactMe card

--- src/components/ContactMe/index.jsx
+++ src/components/ContactMe/index.jsx
@@ -1,16 +1,32 @@
 import { Box } from "@mui/system";
-import { container } from "./styles";
+import { containerDesktop, containerMobile } from "./styles";
 import React, { useState } from "react";
-import { Button, Snackbar, IconButton } from "@mui/material";
+import { Button, Snackbar, IconButton, Typography, Grid } from "@mui/material";
+import { Formik, Form, Field } from "formik";
+import { string } from "yup";
+import PropTypes from "prop-types";
 
-export default function ContactMe() {
+export default function ContactMe(props) {
   // TODO redesign this whole form to be more modern use yup/formik
   // name, email , subject, body
   return (
     <>
-      <Box sx={container}>
-
+      <Box sx={props.isMobile ? containerMobile : containerDesktop}>
+        <Typography
+          variant="h3"
+          sx={{
+            fontFamily: "Edu TAS Beginner, cursive",
+            color: "#D8DEE9",
+            padding: "10px",
+          }}
+        >
+          Contact Me
+        </Typography>
       </Box>
     </>
   );
 }
+
+ContactMe.propTypes = {
+  isMobile: PropTypes.string,
+};
