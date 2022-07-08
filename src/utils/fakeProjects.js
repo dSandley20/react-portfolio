4d3cbc29c81efbb885d064d2ae0252ea35d0e559
dSandley20 <danielsandley2@gmail.com>
2022-07-08 16:47:25 -0400

    data for Project and ProjectScroll components

---
+++ src/utils/fakeProjects.js
@@ -0,0 +1,28 @@
+const projects = [
+    {
+        image: "google.com",
+        title: 'projectOne'
+    },
+    {
+        image: "google.com",
+        title: 'projectTwo'
+    },
+    {
+        image: "google.com",
+        title: 'projectThree'
+    },
+    {
+        image: "google.com",
+        title: 'projectFour'
+    },
+    {
+        image: "google.com",
+        title: 'projectFive'
+    },
+    {
+        image: "google.com",
+        title: 'projectSix'
+    }
+]
+
+module.exports = projects
