f5d8877483f54f0ea817b35a23ec9c0caaaee7cd
dSandley20 <danielsandley2@gmail.com>
2022-07-11 16:12:08 -0400

    rename Skills components to SkillBar

--- src/components/Skills/index.jsx
+++
@@ -1,9 +0,0 @@
-import { containerDesktop, containerMobile } from "./styles";
-import { Box } from "@mui/material";
-export default function Skills(props) {
-  return (
-    <>
-      <Box sx={props.isMobile ? containerMobile : containerDesktop}></Box>
-    </>
-  );
-}
