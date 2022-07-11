85c9d4b10dbd1a5bded94ab608a3e57926275f9a
dSandley20 <danielsandley2@gmail.com>
2022-07-11 16:13:03 -0400

    rename Skills component

---
+++ src/components/SkillBar/index.jsx
@@ -0,0 +1,45 @@
+import { containerDesktop, containerMobile } from "./styles";
+import { Box, Stack } from "@mui/material";
+import Skill from "./Skill";
+
+export default function SkillBar(props) {
+  //TODO pass in technologies
+  console.log(props);
+
+  return (
+    <>
+      <Box sx={props.isMobile ? containerMobile : containerDesktop}>
+        <Stack
+          spacing={4}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          sx={{
+            width: "100%",
+            height: "60px",
+            overflowY: "hidden",
+            display: "-webkit-box",
+            "&::-webkit-scrollbar": {
+              width: ".5em",
+              height: ".3em",
+            },
+            "&::-webkit-scrollbar-track": {
+              borderRadius: "100vw",
+              background: "rgba(255,255,255,0.3)",
+              marginLeft: "1em",
+              marginRight: "1em",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              borderRadius: "100vw",
+              backgroundColor: "rgba(255,255,255,1)",
+            },
+          }}
+        >
+          {props.projects.map((data) => {
+            return <Skill isMobile={props.isMobile} />;
+          })}
+        </Stack>
+      </Box>
+    </>
+  );
+}
---
+++ src/components/SkillBar/styles.js
@@ -0,0 +1,31 @@
+
+
+const containerDesktop = {
+    borderRadius: "1rem",
+    borderColor: '#8FBCBB',
+    marginTop: '10px !important',
+    borderStyle: 'solid',
+    borderWidth: '2px',
+    width: '99%',
+    height: '60px',
+    position: 'absolute',
+    margin: '0 auto',
+    boxShadow: "0px 10px 30px rgb(0,0,0,0.5)",
+   
+}
+
+const containerMobile = {
+    borderRadius: "1rem",
+    borderColor: '#8FBCBB',
+    marginTop: '10px !important',
+    borderStyle: 'solid',
+    borderWidth: '2px',
+    height: '60px',
+    width: '99%',
+    position: 'absolute',
+    margin: '0 auto',
+    boxShadow: "0px 10px 30px rgb(0,0,0,0.5)",
+   
+}
+
+module.exports = {containerDesktop, containerMobile}
