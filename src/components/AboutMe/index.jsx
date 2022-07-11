af834d1e5dda1903f81054b1ae0522190d2a7546
dSandley20 <danielsandley2@gmail.com>
2022-07-11 14:29:29 -0400

    finish up AboutMe component

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -1,19 +1,23 @@
 import { Box, Typography } from "@mui/material";
 import { containerDesktop } from "./styles";
 import TypeAnimation from "react-type-animation";
-import { useState } from "react";
+import ReactTypingEffect from "react-typing-effect";
+import { useState, useEffect } from "react";
+import Typewriter from "typewriter-effect";
+import { SettingsEthernet } from "@mui/icons-material";
 
 export default function AboutMe(props) {
-  const [timeRemaining, setTimeRemaining] = useState(30);
-  const [isAnimating, setIsAnimating] = useState(true);
-  var downloadTimer = setInterval(function () {
-    if (timeRemaining <= 0) {
-      clearInterval(downloadTimer);
-      setIsAnimating(false);
-    }
-    setTimeRemaining(timeRemaining - 1);
-  }, 1000);
+  var text = [
+    `In 2001 my family got our first computer, an old gateway that was
+    running Windows98 with dial up internet. This started my passion for
+    most things tech and experimenting where I would end up in the tech
+    industry from an early age. Figuring out how to use Flash to create
+    animations, to learning C++ to attempt to learn game development before learning HTML / JS / CSS and landing in web
+    development.`,
+    `Since graduating college and moving into a professional software engineer role I have worked with frontend technologies such as React, Angular, and Ember. While I still mostly use node-express for the backend, I have been slowly switching over to Java Spring and DotNet WebApi for my personal projects and want to make that a core part of my skill set.`,
+  ];
 
+  const [isAnimating, setIsAnimating] = useState(false);
   return (
     <>
       <Box sx={containerDesktop}>
@@ -28,25 +32,37 @@ export default function AboutMe(props) {
           About Me
         </Typography>
 
-        <TypeAnimation
-          className="animationStyle"
-          cursor={false}
-          sequence={[
-            `In 2001 my family got our first computer, an old gateway that was running Windows98 with dial up internet. This started my passion for most things tech and experimenting where I would end up in the tech industry from an early age. Figuring out how to use Flash to create animations, to learning C++ to attempt to learn game development I finally settled on learning HTML / JS / CSS and landing in web development.`,
-          ]}
-          wrapper="p"
+        <Typewriter
+          autoStart={true}
+          cursor={"|"}
+          delay={5}
+          onInit={(typewriter) => {
+            typewriter
+              .typeString(text[0])
+              .callFunction(() => {
+                setIsAnimating(true);
+              })
+              .start();
+          }}
         />
 
-        <TypeAnimation
-          className="animationStyle"
-          cursor={!isAnimating}
-          sequence={[
-            "",
-            30000,
-            `Since graduating college and moving into a professional software engineer role I have worked with frontend technologies such as React, Angular, and Ember. While I still mostly use node-express for the backend, I have been slowly switching over to Java Spring and DotNet WebApi for my personal projects and want to make that a core part of my skill set.`,
-          ]}
-          wrapper="p"
-        />
+        {isAnimating && (
+          <Typewriter
+            autoStart={true}
+            cursor={"|"}
+            delay={5}
+            wrapperClassName="typeWritter"
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("")
+                .typeString(text[1])
+                .callFunction(() => {
+                  console.log("String typed out!");
+                })
+                .start();
+            }}
+          />
+        )}
       </Box>
     </>
   );
